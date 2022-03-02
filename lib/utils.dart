5b664535e79a6b348c91445f21481968c44b74f9
luca <lucabonasera2@gmail.com>
2022-03-02 12:50:30 +0100

    remove utils.dart

--- lib/utils.dart
+++
@@ -1,7 +0,0 @@
-  import 'package:flutter/material.dart';
-
-  
-  void _snackBar(BuildContext context, String message) {
-    final snakBar = SnackBar(content: Text(message));
-    ScaffoldMessenger.of(context).showSnackBar(snakBar);
-  }
